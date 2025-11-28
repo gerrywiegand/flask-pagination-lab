bfd8579309943c579b424cb77f19134ef232bf38
Gerry <gerrywiegand@gmail.com>
2025-11-27 19:56:27 -0500

    finished

--- server/app.py
+++ server/app.py
@@ -1,24 +1,35 @@
 #!/usr/bin/env python3
 
-from flask import request, session, jsonify, make_response
-from flask_restful import Resource
-from sqlalchemy.exc import IntegrityError
-
 import os
-from config import create_app, db, api
+
+from config import api, create_app, db
+from flask import request
+from flask_restful import Resource
 from models import Book, BookSchema
+
+
+class Books(Resource):
+    def get(self):
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 5, type=int)
+
+        pagination = Book.query.paginate(page=page, per_page=per_page, error_out=False)
+
+        books = pagination.items
+        return {
+            "page": page,
+            "per_page": per_page,
+            "total": pagination.total,
+            "total_pages": pagination.pages,
+            "items": BookSchema(many=True).dump(books),
+        }, 200
+
+
+api.add_resource(Books, "/books", endpoint="books")
 
 env = os.getenv("FLASK_ENV", "dev")
 app = create_app(env)
 
-class Books(Resource):
-    def get(self):
-        books = [BookSchema().dump(b) for b in Book.query.all()]
-        return books, 200
 
-
-api.add_resource(Books, '/books', endpoint='books')
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
